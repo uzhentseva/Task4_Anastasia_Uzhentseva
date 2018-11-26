d9e34cc8fd5623acd98e86cf9e7cc2e4d7820e48
Anastasia Uzhentseva <uzhentseva@yahoo.com>
2018-11-26 16:04:28 +0300

    Initial Commit

---
+++ Task4_Hw2_Uzhentseva/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Task4_Hw2_Uzhentseva
+//
+//  Created by Uzh on 26/11/2018.
+//  Copyright © 2018 Uzh. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
