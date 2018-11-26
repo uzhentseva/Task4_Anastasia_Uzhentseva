af03395dedfe712bc1ed7c6120a1c2e135f40899
Anastasia Uzhentseva <uzhentseva@yahoo.com>
2018-11-26 16:06:33 +0300

    Update main.swift

--- Task4_Hw2_Uzhentseva/main.swift
+++ Task4_Hw2_Uzhentseva/main.swift
@@ -1,12 +1,23 @@
-//
-//  main.swift
-//  Task4_Hw2_Uzhentseva
-//
-//  Created by Uzh on 26/11/2018.
-//  Copyright © 2018 Uzh. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+//Задание 3
+//массив чисел от 0 до 100
+var arrayHundred = [Int]()
+for i in 0...100 {
+    arrayHundred.append(i)
+}
 
+//Задание 4
+//удаляем из массива все числа кратные 2 и 3
+//точнее, создаём новые массивы с числами не кратными 2 и 3
+var array100 = [Int]()
+for i in arrayHundred where i % 2 != 0 {
+    array100.append(i)
+}
+print(array100)
+
+var array3 = [Int]()
+for i in arrayHundred where i % 3 != 0 {
+    array3.append(i)
+}
+print(array3)
